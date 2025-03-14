285708ec3440b5fb5702f9899f4cea4af4c5ee99
Bekmurod <bekmruodgofurov0000@gmail.com>
2025-03-14 09:28:45 +0500

    forEach method

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 let inputBox = document.querySelector("#inputBox");
 let result = document.querySelector("#result")
 let equal = document.querySelector("#equal")
-let num1 = document.querySelector("#num1")
-let num2 = document.querySelector("#num2")
-let num3 = document.querySelector("#num3")
-let num4 = document.querySelector("#num4")
-let num5 = document.querySelector("#num5")
-let num6 = document.querySelector("#num6")
-let num7 = document.querySelector("#num7")
-let num8 = document.querySelector("#num8")
-let num9 = document.querySelector("#num9")
-let num0 = document.querySelector("#num0")
 let devision = document.querySelector("#dev")
-let mult = document.querySelector("#mult")
-let dec = document.querySelector("#dec")
-let add = document.querySelector("#add")
-let point = document.querySelector("#point")
 let clearAc = document.querySelector("#ac")
+
+document.querySelectorAll('.num, .oparator').forEach(button => {
+  button.addEventListener("click", function(){
+    inputBox.value += this.textContent;
+  });
+});
 
 
 equal.addEventListener("click", function(){
@@ -34,52 +26,6 @@ equal.addEventListener("click", function(){
     alert.remove();
   }, 5000);
   }
-})
-
-num0.addEventListener("click", function(){
-  inputBox.value += "0"
-})
-num1.addEventListener("click", function(){
-  inputBox.value += "1"
-})
-num2.addEventListener("click", function(){
-  inputBox.value += "2"
-})
-num3.addEventListener("click", function(){
-  inputBox.value += "3"
-})
-num4.addEventListener("click", function(){
-  inputBox.value += "4"
-})
-num5.addEventListener("click", function(){
-  inputBox.value += "5"
-})
-num6.addEventListener("click", function(){
-  inputBox.value += "6"
-})
-num7.addEventListener("click", function(){
-  inputBox.value += "7"
-})
-num8.addEventListener("click", function(){
-  inputBox.value += "8"
-})
-num9.addEventListener("click", function(){
-  inputBox.value += "9"
-})
-
-devision.addEventListener("click", function(){
-  inputBox.value += "/"
-})
-mult.addEventListener("click", function(){
-  inputBox.value += "*"
-})
-dec.addEventListener("click", function(){
-  inputBox.value += "-"
-})
-add.addEventListener("click", function(){
-  inputBox.value += "+"
-  console.log("add");
-  
 })
 
 clearAc.addEventListener("click", function(){
